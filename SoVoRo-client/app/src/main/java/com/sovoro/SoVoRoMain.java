a9ef0e0a4402f346b441083aed7401c18127529c
201710787 <egnachio@naver.com>
2022-05-03 18:32:48 +0900

    edit main page

--- SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoMain.java
+++ SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoMain.java
@@ -1,14 +1,37 @@
 package com.sovoro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SoVoRoMain extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.sovoro_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sovoro_main);
+
+        toolbar = (Toolbar) findViewById(R.id.sovoro_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
