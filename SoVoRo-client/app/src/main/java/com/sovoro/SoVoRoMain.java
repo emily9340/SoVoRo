748d40615f95a62017ad5e45711e93852077d36c
201710787 <66404645+egnachio@users.noreply.github.com>
2022-05-04 02:00:44 +0900

    add toolbar and drawer

--- SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoMain.java
+++ SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoMain.java
@@ -1,18 +1,25 @@
 package com.sovoro;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class SoVoRoMain extends AppCompatActivity {
 
     private Toolbar toolbar;
-
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //return super.onCreateOptionsMenu(menu);
@@ -23,6 +30,12 @@ public class SoVoRoMain extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -33,5 +46,14 @@ public class SoVoRoMain extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.sovoro_toolbar);
         setSupportActionBar(toolbar);
+        // 왼쪽 메뉴바 삽입
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+        // 타이틀 제거
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawer=findViewById(R.id.sovoro_drawer);
+        navigationView=findViewById(R.id.sovoro_drawer_view);
+        
     }
 }
