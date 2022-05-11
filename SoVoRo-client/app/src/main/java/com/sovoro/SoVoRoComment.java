69cf99723c5b996f11601fc9fc2f6dcb131bb939
201710787 <66404645+egnachio@users.noreply.github.com>
2022-05-11 14:13:21 +0900

    comment page

--- SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoComment.java
+++ SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoComment.java
@@ -1,14 +1,84 @@
 package com.sovoro;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class SoVoRoComment extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
+    private Button sovoroCommentSubmitButton;
+    private EditText sovoroCommentInput;
+    private RecyclerView sovoroComments;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.sovoro_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sovoro_comment);
+
+        ArrayList<SoVoRoCommentInfo> alist=new ArrayList<>();
+
+        sovoroCommentSubmitButton=findViewById(R.id.sovoro_comment_submit);
+        sovoroCommentInput=findViewById(R.id.sovoro_comment_input);
+        sovoroComments=findViewById(R.id.sovoro_comments);
+
+        toolbar = (Toolbar) findViewById(R.id.sovoro_comment_toolbar);
+        setSupportActionBar(toolbar);
+        // 왼쪽 메뉴바 삽입
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);
+        // 타이틀 제거
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        drawer=findViewById(R.id.sovoro_comment_drawer);
+        navigationView=findViewById(R.id.sovoro_comment_drawer_view);
+
+        sovoroCommentSubmitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String txt=sovoroCommentInput.getText().toString();
+                SoVoRoCommentInfo sovoRoCommentInfo=new SoVoRoCommentInfo("0",txt,"0");
+                alist.add(sovoRoCommentInfo);
+                SoVoRoCommentAdapter sovoroCommentAdapter =new SoVoRoCommentAdapter(alist);
+                sovoroComments.setAdapter(sovoroCommentAdapter);
+            }
+        });
+
     }
 }
