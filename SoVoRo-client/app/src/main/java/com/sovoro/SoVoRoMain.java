065b8058a52f758623a654e4b6332ff9ece2e724
201710787 <egnachio@naver.com>
2022-05-15 23:04:20 +0900

    fragment

--- SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoMain.java
+++ SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoMain.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.navigation.NavigationView;
+
+import me.relex.circleindicator.CircleIndicator3;
 
 public class SoVoRoMain extends AppCompatActivity {
 
@@ -20,6 +26,13 @@ public class SoVoRoMain extends AppCompatActivity {
     private DrawerLayout drawer;
     private ActionBarDrawerToggle toggle;
     private NavigationView navigationView;
+
+    // 뷰페이저
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 2;
+    private CircleIndicator3 mIndicator;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //return super.onCreateOptionsMenu(menu);
@@ -55,5 +68,56 @@ public class SoVoRoMain extends AppCompatActivity {
         drawer=findViewById(R.id.sovoro_main_drawer);
         navigationView=findViewById(R.id.sovoro_main_drawer_view);
 
+        //ViewPager2
+        mPager = findViewById(R.id.sovoro_main_viewpager);
+        //Adapter
+        pagerAdapter = new SoVoRoWordAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.sovoro_main_indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(1000);
+        mPager.setOffscreenPageLimit(3);
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
+            }
+
+        });
+
+
+        final float pageMargin= getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
+
+        mPager.setPageTransformer(new ViewPager2.PageTransformer() {
+
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float myOffset = position * -(2 * pageOffset + pageMargin);
+                if (mPager.getOrientation() == ViewPager2.ORIENTATION_HORIZONTAL) {
+                    if (ViewCompat.getLayoutDirection(mPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                        page.setTranslationX(-myOffset);
+                    } else {
+                        page.setTranslationX(myOffset);
+                    }
+                } else {
+                    page.setTranslationY(myOffset);
+                }
+            }
+        });
     }
 }
---
+++ SoVoRo-client/app/src/main/java/com/sovoro/SoVoRoWordAdapter.java
@@ -0,0 +1,43 @@
+package com.sovoro;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.savedstate.SavedStateRegistryOwner;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class SoVoRoWordAdapter extends FragmentStateAdapter {
+    public int mCount;
+    public SoVoRoWordAdapter(@NonNull FragmentActivity fragmentActivity, int _mCount) {
+        super(fragmentActivity);
+        mCount=_mCount;
+    }
+//    public SoVoRoWordAdapter(@NonNull Fragment fragment) {
+//        super(fragment);
+//    }
+//
+//    public SoVoRoWordAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+//        super(fragmentManager, lifecycle);
+//    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        int index = getRealPosition(position);
+        switch (index) {
+            case 1:
+                return SoVoRoWordFragment1.newInstance("1","a");
+            case 2:
+                return SoVoRoWordFragment2.newInstance("2","b");
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+    public int getRealPosition(int position) { return position % mCount; }
+}
